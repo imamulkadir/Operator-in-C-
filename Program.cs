15973284c6be4df5bf9dfb864137f27c4dd02bf6
Imamul Kadir <imamulkadiroyan@gmail.com>
2022-12-09 03:21:39 +0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            } else if(op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            } else if(op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            } else if(op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+        }
+    }
+}
